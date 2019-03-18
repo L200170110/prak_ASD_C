b0c07689e24eaf9c06e1307fe43721e6219a2068
L200170110 <43409244+L200170110@users.noreply.github.com>
2019-03-18 21:25:42 +0700

    Add files via upload

---
+++ modul-2/1A.py
@@ -0,0 +1,28 @@
+
+class Pesan(object):
+    """
+        Sebuah class bernama Pesan.
+        Untuk memahami konsep Class dan Object.
+    """
+    def __init__(self, sebuahString):
+        self.teks = sebuahString
+    def cetakIni(self):
+        print(self.teks)
+    def cetakPakaiHurufKapital(self):
+        print(str.upper(self.teks))
+    def cetakPakaiHurufKecil(self):
+        print(str.lower(self.teks))
+    def jumKar(self):
+        return len(self.teks)
+    def cetakJumlahKarakterku(self):
+        print('Kalimatku mempunyai',len(self.teks),'karakter.')
+    def perbarui(self,stringBaru):
+        self.teks = stringBaru
+# 1 (A)
+    def apakahTerkandung(self, kata):
+        self.kata = kata
+        if self.kata in self.teks:
+            return True
+        else:
+            return False
+
---
+++ modul-2/6.py
@@ -0,0 +1,34 @@
+class Manusia(object):
+    keadaan="lapar"
+    def __init__(self,nama):
+        self.nama=nama
+    def ucapkanSalam(self):
+        print("Salam, namaku", self.nama)
+    def makan(self,s):
+        print("Saya baru saja makan", s)
+        self.keadaan="kenyang"
+    def olahraga(self,k):
+        print("Saya baru saja latihan", k)
+        self.keadaan="lapar"
+    def mengalikanDenganDua(self,n):
+        return n*2
+    
+class SiswaSMA(Manusia):
+    """Class SiswaSMA yang dibangun dari class Manusia"""
+    def __init__(self,nama,no_induk,kelas,alamat):
+        """Metode inisialisasi ini menutupi metode inisiasi di class Manusia."""
+        self.nama = nama
+        self.no = no_absen
+        self.kelas = kelas
+        self.alamat = alamat
+    def __str__(self):
+        a = "Nama : "+self.nama+'\n'+"No Absen : "+str(self.no)+'\n'+"Asal dari : "+self.alamat+'\n'+"Kelas : "+str(self.kelas)
+        print (a)
+    def ambilNama(self):
+        print (self.nama)
+    def ambilNoInduk(self):
+        print (self.no)
+    def ambilKelas(self):
+        print (self.kelas)
+    def ambilAlamat(self):
+        print (self.alamat)
